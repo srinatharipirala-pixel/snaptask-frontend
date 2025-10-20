d122bd3506429eafed252d30ab91ed7bd91ee6cf
srinatharipirala-pixel <srinath.aripirala@gmail.com>
2025-10-20 15:08:57 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
